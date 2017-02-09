b1ca6edf2635470929d5f3cf869e26971dcb420d
Glomberg <Bazz@bk.ru>
2017-02-09 16:27:54 +0300

    5

--- www/js/script.js
+++ www/js/script.js
@@ -1,12 +1,26 @@
-﻿$(document).ready(function(){
+﻿function fix_menu_on() {
+	$("header nav").addClass("fixed");
+	$("header").css("padding-bottom", "71px");
+}
+function fix_menu_off() {
+	$("header nav").removeAttr("class");
+	$("header").removeAttr("style");
+}
+
+$(document).ready(function(){
 	// Sticky menu
+	if($(document).scrollTop() > 115) {
+		fix_menu_on()
+	} else {
+		fix_menu_off()
+	}
 	$(window).scroll(function(){
 		if($(document).scrollTop() > 115) {
-			$("header nav").addClass("fixed");
-			$("header").css("padding-bottom", "71px");
+			fix_menu_on()
 		} else {
-			$("header nav").removeAttr("class");
-			$("header").removeAttr("style");
+			fix_menu_off()
 		}
 	});
+	// dotdotdot
+    $(".goods .item .description").dotdotdot();
 });
