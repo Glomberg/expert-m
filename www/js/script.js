7b648a26a60e943bf5038761e2908f798c3e2791
Glomberg <Bazz@bk.ru>
2017-02-10 15:25:04 +0300

    6

--- www/js/script.js
+++ www/js/script.js
@@ -8,6 +8,35 @@ function fix_menu_off() {
 }
 
 $(document).ready(function(){
+	// Animate placeholders
+	if ($('.animated-placeholder').length >= 1) {
+		var hidden_styles = {'opacity': '0', 'left': '-65px'};
+		$('*[placeholder]').each(function(){
+			if ($(this).next().is('label')) {
+				if(!$(this).val() == '') {
+					$(this).next().css(hidden_styles);
+				}
+				$(this).next().text($(this).attr('placeholder'));
+				$(this).attr('placeholder', '');
+			}
+		});
+		$('.animated-placeholder input, .animated-placeholder textarea').focus(function(){
+			$(this).next().css(hidden_styles);
+		}).blur(function(){
+			if($(this).val() == '') {
+				$(this).next().removeAttr('style');
+			}
+		});
+	}
+	// Mini-Cart opener
+	$('.header-basket-col .basket').on('click', function(){
+		if($('.header-basket-col .basket-details').is(':visible')) {
+			$('.header-basket-col .basket-details').slideUp();
+		} else {
+			$('.header-basket-col .basket-details').slideDown();
+			if($(".basket-details-body .item .title").length >= 1) { $(".basket-details-body .item .title").dotdotdot(); }
+		}
+	});
 	// Sticky menu
 	if($(document).scrollTop() > 115) {
 		fix_menu_on()
@@ -22,5 +51,10 @@ $(document).ready(function(){
 		}
 	});
 	// dotdotdot
-    $(".goods .item .description").dotdotdot();
+	if($(".goods .item .description").length >= 1) { $(".goods .item .description").dotdotdot(); }
+	//Mini-Cart remove item
+	$('.basket-details-body .item .remove a').on('click', function(e){
+		e.preventDefault();
+		$(this).parent().parent('.item').slideUp();
+	});
 });
