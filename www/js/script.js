430021e2a945d61799ab7929e5bef2f8c0870f77
Viktor <bazz@bk.ru>
2017-02-16 23:17:15 +0300

    12.5

--- www/js/script.js
+++ www/js/script.js
@@ -182,4 +182,32 @@ $(document).ready(function(){
 			}
 		});
 	}
+	// Datepicker
+	if($("#datepicker").length >= 1) {
+		$.datepicker.regional['ru'] = {
+			closeText: 'Закрыть',
+			prevText: '&#x3c;Пред',
+			nextText: 'След&#x3e;',
+			currentText: 'Сегодня',
+			monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+			'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+			monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+			'Июл','Авг','Сен','Окт','Ноя','Дек'],
+			dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+			dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+			dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+			weekHeader: 'Нед',
+			dateFormat: 'dd.mm.yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''};
+		$.datepicker.setDefaults($.datepicker.regional['ru']);
+		$("#datepicker").datepicker($.extend({
+			showOn: "button",
+			buttonImage: "images/calendar.png",
+			buttonImageOnly: true,
+			buttonText: "Выберите дату"
+		},$.datepicker.regional['ru']));
+	}
 });
